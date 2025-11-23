6503df09d7815018337b27c947795148cee83518
Wit-2k <skeialo@163.com>
2025-11-23 22:20:06 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStartPause = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.rbtnShortBreak = new System.Windows.Forms.RadioButton();
+            this.rbtnLongBreak = new System.Windows.Forms.RadioButton();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.lblCurrentTime = new System.Windows.Forms.Label();
+            this.tmrClock = new System.Windows.Forms.Timer(this.components);
+            this.pgbTimer = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("微软雅黑", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblStatus.Location = new System.Drawing.Point(584, 111);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(153, 78);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "准备";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("微软雅黑", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTime.Location = new System.Drawing.Point(554, 238);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(315, 131);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "25:00";
+            // 
+            // btnStartPause
+            // 
+            this.btnStartPause.Font = new System.Drawing.Font("微软雅黑", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStartPause.Location = new System.Drawing.Point(285, 682);
+            this.btnStartPause.Name = "btnStartPause";
+            this.btnStartPause.Size = new System.Drawing.Size(257, 79);
+            this.btnStartPause.TabIndex = 3;
+            this.btnStartPause.Text = "开始";
+            this.btnStartPause.UseVisualStyleBackColor = true;
+            this.btnStartPause.Click += new System.EventHandler(this.btnStartPause_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("微软雅黑", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnReset.Location = new System.Drawing.Point(875, 682);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(257, 79);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "重新计时";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // rbtnShortBreak
+            // 
+            this.rbtnShortBreak.AutoSize = true;
+            this.rbtnShortBreak.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rbtnShortBreak.Location = new System.Drawing.Point(403, 516);
+            this.rbtnShortBreak.Name = "rbtnShortBreak";
+            this.rbtnShortBreak.Size = new System.Drawing.Size(168, 40);
+            this.rbtnShortBreak.TabIndex = 4;
+            this.rbtnShortBreak.TabStop = true;
+            this.rbtnShortBreak.Text = "休息5分钟";
+            this.rbtnShortBreak.UseVisualStyleBackColor = true;
+            this.rbtnShortBreak.CheckedChanged += new System.EventHandler(this.rbtnShortBreak_CheckedChanged);
+            // 
+            // rbtnLongBreak
+            // 
+            this.rbtnLongBreak.AutoSize = true;
+            this.rbtnLongBreak.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rbtnLongBreak.Location = new System.Drawing.Point(855, 516);
+            this.rbtnLongBreak.Name = "rbtnLongBreak";
+            this.rbtnLongBreak.Size = new System.Drawing.Size(184, 40);
+            this.rbtnLongBreak.TabIndex = 4;
+            this.rbtnLongBreak.TabStop = true;
+            this.rbtnLongBreak.Text = "休息15分钟";
+            this.rbtnLongBreak.UseVisualStyleBackColor = true;
+            this.rbtnLongBreak.CheckedChanged += new System.EventHandler(this.rbtnLongBreak_CheckedChanged);
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Enabled = true;
+            this.tmrMain.Interval = 1000;
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // lblCurrentTime
+            // 
+            this.lblCurrentTime.AutoSize = true;
+            this.lblCurrentTime.Font = new System.Drawing.Font("黑体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblCurrentTime.Location = new System.Drawing.Point(36, 25);
+            this.lblCurrentTime.Name = "lblCurrentTime";
+            this.lblCurrentTime.Size = new System.Drawing.Size(87, 36);
+            this.lblCurrentTime.TabIndex = 5;
+            this.lblCurrentTime.Text = "time";
+            // 
+            // tmrClock
+            // 
+            this.tmrClock.Enabled = true;
+            this.tmrClock.Interval = 1000;
+            this.tmrClock.Tick += new System.EventHandler(this.tmrClock_Tick);
+            // 
+            // pgbTimer
+            // 
+            this.pgbTimer.Location = new System.Drawing.Point(109, 438);
+            this.pgbTimer.Name = "pgbTimer";
+            this.pgbTimer.Size = new System.Drawing.Size(1249, 10);
+            this.pgbTimer.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 904);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnStartPause);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.rbtnShortBreak);
+            this.Controls.Add(this.rbtnLongBreak);
+            this.Controls.Add(this.lblCurrentTime);
+            this.Controls.Add(this.pgbTimer);
+            this.Name = "Form1";
+            this.Text = "简易番茄钟";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStartPause;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.RadioButton rbtnShortBreak;
+        private System.Windows.Forms.RadioButton rbtnLongBreak;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.Label lblCurrentTime;
+        private System.Windows.Forms.Timer tmrClock;
+        private System.Windows.Forms.ProgressBar pgbTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Windows.Forms;
+
+// TODO：自定义工作时间和休息时间
+// TODO：增加系统托盘图标
+// TODO：退出程序前提示今天完成的番茄钟数量
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        #region 1. 常量、变量
+
+        // 调试用
+        private const int TIME_WORK = 10;
+        private const int TIME_SHORT_BREAK = 12;
+        private const int TIME_LONG_BREAK = 15;
+
+        //private const int TIME_WORK = 25 * 60; // 工作时间25分钟
+        //private const int TIME_SHORT_BREAK = 5 * 60; // 短休息时间5分钟
+        //private const int TIME_LONG_BREAK = 15 * 60; // 长休息时间15分钟
+
+        private int totalSeconds = TIME_WORK;
+        private int currentSeconds; // 剩余时间
+
+        private enum PomodoroMode // 番茄钟模式决定 totalSeconds 变量的值
+        {
+            Work,
+            ShortBreak,
+            LongBreak
+        }
+
+        private PomodoroMode currentMode = PomodoroMode.Work;
+        private PomodoroMode breakMode; // 由单选框决定休息模式
+
+        private enum TimerState // 计时器状态决定标签和按钮文本
+        {
+            Stopped,
+            Running,
+            Paused
+        }
+
+        private TimerState timerState = TimerState.Stopped;
+
+        #endregion
+
+        #region 2. 初始化与加载
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.FixedSingle; // 禁止缩放
+            MaximizeBox = false; // 禁止最大化            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetTimer();
+            lblStatus.Text = "准备工作"; // 每次打开默认准备工作
+            rbtnShortBreak.Checked = true; // 默认短休息
+            lblCurrentTime.Text = DateTime.Now.ToString("HH:mm:ss"); // 显示当前时间，消除启动延迟
+        }
+
+        #endregion
+
+        #region 3. 业务逻辑
+
+        private void ResetTimer()
+        {
+            tmrMain.Stop();
+            timerState = TimerState.Stopped;
+            currentSeconds = totalSeconds;
+
+            pgbTimer.Maximum = totalSeconds;
+            pgbTimer.Value = totalSeconds;
+
+            btnStartPause.Text = "开始";
+            RefreshLabel();
+        }
+
+        private void RefreshLabel()  // 刷新时间标签，主要由定时器触发
+        {
+            lblTime.Text = string.Format("{0:D2}:{1:D2}", currentSeconds / 60, currentSeconds % 60);
+        }
+
+        private void SwitchMode(PomodoroMode newMode)
+        {
+            currentMode = newMode; // 自动或手动改变模式
+
+            switch (currentMode)
+            {
+                case PomodoroMode.Work:
+                    totalSeconds = TIME_WORK;
+                    lblStatus.Text = "准备工作";
+                    break;
+                case PomodoroMode.ShortBreak:
+                    totalSeconds = TIME_SHORT_BREAK;
+                    lblStatus.Text = "准备休息（短）";
+                    break;
+                case PomodoroMode.LongBreak:
+                    totalSeconds = TIME_LONG_BREAK;
+                    lblStatus.Text = "准备休息（长）";
+                    break;
+            }
+
+            ResetTimer();
+        }
+
+        #endregion
+
+        #region 4. UI事件
+
+        /// <summary>
+        /// “重新计时”按钮不仅重置时间，还要刷新状态标签文本
+        /// </summary>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            SwitchMode(currentMode); // 重置定时器，刷新标签内容
+        }
+
+        /// <summary>
+        /// 定时期间更新时间标签和进度条
+        /// 定时结束后弹出提示框，切换模式并重置定时器
+        /// </summary>
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            if (currentSeconds > 0) // 该部分每秒执行一次
+            {
+                currentSeconds--;
+                RefreshLabel();
+                if (currentSeconds <= pgbTimer.Maximum)
+                {
+                    pgbTimer.Value = currentSeconds; // 更新进度条
+                }
+            }
+            else // 时间到
+            {
+                tmrMain.Stop();
+                timerState = TimerState.Stopped;
+                btnStartPause.Text = "开始"; // 立即更新提示文本，避免轮询
+
+                if (currentMode == PomodoroMode.Work) // 一段工作时间结束
+                {
+                    MessageBox.Show("时间到，休息一下吧！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    SwitchMode(breakMode);
+                }
+                else // 一段休息时间结束
+                {
+                    MessageBox.Show("时间到，准备工作吧！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    SwitchMode(PomodoroMode.Work);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 开始/暂停/继续按钮
+        /// 由定时器状态决定显示文本和标签内容
+        /// </summary>
+        private void btnStartPause_Click(object sender, EventArgs e)
+        {
+            if (timerState == TimerState.Running)
+            {
+                tmrMain.Stop();
+                timerState = TimerState.Paused;
+                btnStartPause.Text = "继续"; // 定时器等待继续
+                lblStatus.Text = "已暂停";
+            }
+            else
+            {
+                tmrMain.Start();
+                timerState = TimerState.Running;
+                btnStartPause.Text = "暂停"; // 定时器等待暂停
+                switch (currentMode)
+                {
+                    case PomodoroMode.Work:
+                        lblStatus.Text = "专注中...";
+                        break;
+                    default:
+                        lblStatus.Text = "休息中...";
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计时中切换休息模式在计时结束后生效
+        /// 计时结束后可自由更改休息模式，同步更新状态标签文本
+        /// </summary>
+        private void rbtnShortBreak_CheckedChanged(object sender, EventArgs e)
+        {
+            breakMode = PomodoroMode.ShortBreak;
+
+            if (timerState == TimerState.Stopped && currentMode != PomodoroMode.Work)
+            {
+                SwitchMode(PomodoroMode.ShortBreak);
+            }
+        }
+
+        /// <summary>
+        /// 计时中切换休息模式在计时结束后生效
+        /// 计时结束后可自由更改休息模式，同步更新状态标签文本
+        /// </summary>
+        private void rbtnLongBreak_CheckedChanged(object sender, EventArgs e)
+        {
+            breakMode = PomodoroMode.LongBreak;
+
+            if (timerState == TimerState.Stopped && currentMode != PomodoroMode.Work)
+            {
+                SwitchMode(PomodoroMode.LongBreak);
+            }
+        }
+
+        private void tmrClock_Tick(object sender, EventArgs e)
+        {
+            lblCurrentTime.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        #endregion
+
+    }
+}
